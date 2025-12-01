53708bd4aa42171198c22ba27356087ec905c755
vinoxindonesia33-ops <vinoxindonesia33@gmail.com>
2025-12-01 21:01:53 +0100

    install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+clear
+echo "Installing dependencies..."
+pkg update -y
+pkg upgrade -y
+pkg install python git -y
+
+echo "Setting up VINOX TOOL..."
+chmod +x vinox.py
+
+echo "Creating command shortcut..."
+cp vinox.py $PREFIX/bin/vinox
+chmod +x $PREFIX/bin/vinox
+
+echo
+echo "===================================="
+echo "   ✔ Installation Complete"
+echo "   Run tool using: vinox"
+echo "===================================="
